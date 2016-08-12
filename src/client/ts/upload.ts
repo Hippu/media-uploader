05a42f97a62efe1dbc2445e72409ffecb33406bd
Teemu Heinämäki <teemu.heinamaki@gmail.com>
2016-08-12 21:48:00 +0300

    Initial work on progress bar when uploading

--- src/client/ts/upload.ts
+++ src/client/ts/upload.ts
@@ -3,19 +3,22 @@ import updateList from "./file-list";
 function upload(file: File): void {
     let request = new XMLHttpRequest();
     let formData = new FormData();
+    let progress = document.getElementById("uploadProgress") as HTMLProgressElement;
+    progress.classList.add("visible");
 
     formData.append("filename", file.name);
     formData.append("uploadedFile", file);
 
-    request.onload = function() {
+    request.onload = () => {
         updateList();
+        progress.classList.remove("visible");
     }
-    request.onprogress = function (event: ProgressEvent) {
-      console.log(`${event.loaded} / ${event.total}`)
+    request.onprogress = function(event: ProgressEvent) {
+        progress.value = event.loaded;
+        progress.max = event.total;
     }
     request.open('POST', '/upload', true);
     request.send(formData);
-
 }
 
 export default upload;
